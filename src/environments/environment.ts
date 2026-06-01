a1701a31d7768fada94ab7e47595446ab7f773c2
Gichimu <gichimueric12@gmail.com>
2026-06-01 21:28:58 +0300

    add the new till number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@ export const environment: Environment = {
   apiUrl: 'https://posbackend-production-9320.up.railway.app', // Set this to your production API URL
   storeName: 'CHASESPOT FRYZ',
   storePhone: '0111 435 964',
-  tillNumber: '3095862',
+  // tillNumber: '3095862', // old till number
+  tillNumber: '3437693',
 };
