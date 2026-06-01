6608fecd8aca51eb3017b6065abe98032bd8f11d
Gichimu <gichimueric12@gmail.com>
2026-06-01 21:45:08 +0300

    updating envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'https://posbackend-production-9320.up.railway.app', // Set this to your production API URL
+  // apiUrl: 'https://posbackend-production-9320.up.railway.app', // Set this to your production API URL
+  apiUrl: 'https://pos-backend-9ugb.onrender.com/api', // Set this to your production API URL
   storeName: 'CHASESPOT FRYZ',
   storePhone: '0111 435 964',
   // tillNumber: '3095862', // old till number
