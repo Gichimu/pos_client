7b32ea4c21862251935b4882736d4687bfadc52f
Gichimu <gichimueric12@gmail.com>
2026-06-01 21:12:37 +0300

    change provider to render.com

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,8 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
   production: false,
   // apiUrl: 'http://localhost:3080/api',
-  apiUrl: 'https://posbackend-production-9320.up.railway.app/api',
+  // apiUrl: 'https://posbackend-production-9320.up.railway.app/api',
+  apiUrl: 'https://pos-backend-9ugb.onrender.com/api',
   storeName: 'CHASESPOT FRYZ',
   storePhone: '0111 435 964',
   tillNumber: '3095862',
